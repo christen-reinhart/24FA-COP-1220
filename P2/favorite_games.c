638b2e6a1395f579391d15dec20e60a9ff8f9e95
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-02 19:24:26 -0400

    Games

---
+++ P2/favorite_games.c
@@ -0,0 +1,22 @@
+# !/usr/bin/env Program C
+# Script name: Programming C P2
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 08/30/2024
+# Purpose: Print
+# Input: Video Games
+# Output: Print Video Games
+
+#include <stdio.h>
+
+int main() {
+    printf("Some of my favorite video games are:\n");
+    printf("1. The Legend of Zelda: Breath of the Wild\n");
+    printf("2. God of War\n");
+    printf("3. Red Dead Redemption 2\n");
+    printf("4. The Witcher 3: Wild Hunt\n");
+    printf("5. Halo: Infinite\n");
+    
+    return 0;
+}
+
+# end
