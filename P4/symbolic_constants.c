e672a1cdd90b7ebc4b19fe01281d9e04a8517af0
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-06 20:37:49 -0400

    Polygon

--- P4/symbolic_constants.c
+++ P4/symbolic_constants.c
@@ -1,7 +1,7 @@
 // !/usr/bin/env Program C
 // Script name: Programming C P3 Conversion
 // Author Name: Christen Reinhart
-// Date of Latest Revision: 09/7/2024
+// Date of Latest Revision: 09/13/2024
 // Purpose: Calculate Area 
 // Input: Side 1/2
 // Output: Area
