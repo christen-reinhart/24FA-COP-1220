a06d94d9fdaefb1ee49ce2438a93099e5cc43596
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-09 16:40:05 -0400

    ASCII

--- P3/ASCII-code.c
+++ P3/ASCII-code.c
@@ -12,15 +12,15 @@ int main() {
     int ascii_code;  // Declare a variable to store the ASCII code
 
     // Prompt the user to enter an ASCII code value
-    printf("Enter an ASCII code value: ");
+    printf("Enter ASCII value: ");
     scanf("%d", &ascii_code);
 
     // Output the character that corresponds to the ASCII code
     printf("The character corresponding to ASCII code %d is '%c'.\n", ascii_code, ascii_code);
 
     // Test output for decimal 106 and 122
-    printf("Test: ASCII code 106 corresponds to '%c'.\n", 106);
-    printf("Test: ASCII code 122 corresponds to '%c'.\n", 122);
+    printf("ASCII code 106 goes with '%c'.\n", 106);
+    printf("ASCII code 122 goes with '%c'.\n", 122);
 
     return 0;
 }
