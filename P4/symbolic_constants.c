33efedd547d7a408bc04097f4de9aafecb60be4a
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-15 14:52:35 -0400

    symbolic constant

--- P4/symbolic_constants.c
+++ P4/symbolic_constants.c
@@ -3,7 +3,7 @@
 // Author Name: Christen Reinhart
 // Date of Latest Revision: 09/15/2024
 // Purpose: Calculate Area 
-// Input: Side 1/2
+// Input: Side 1 and 2
 // Output: Area
 
 #include <stdio.h>
@@ -15,17 +15,17 @@ int main() {
     float side1, side2, area;
 
     // input for side1 and side2
-    printf("Enter the value for side 1: ");
+    printf("enter value for side 1: ");
     scanf("%f", &side1);
 
-    printf("Enter the value for side 2: ");
+    printf("enter value for side 2: ");
     scanf("%f", &side2);
 
     // area of the scona
     area = (side1 * FACTOR1) + (side2 * FACTOR2);
 
     // results displayed
-    printf("The area of the scona is: %.2f\n", area);
+    printf("the area is: %.2f\n", area);
 
     return 0;
 }
