6fb41fb49f2cb3551edd41bec3f5b94fab8c289b
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-15 15:15:45 -0400

    goosey

--- P5/goosey.c
+++ P5/goosey.c
@@ -1,7 +1,36 @@
 // !/usr/bin/env Program C
-// Script name: Programming C P3 Conversion
+// Script name: Programming C P5 
 // Author Name: Christen Reinhart
-// Date of Latest Revision: 09/13/2024
-// Purpose: Calculate Area 
-// Input: Side 1/2
-// Output: Area
+// Date of Latest Revision: 09/18/2024
+// Purpose: multiplication 
+// Input: type int value
+// Output: 321 Multiplied
+
+#include <stdio.h>
+
+// symbolic constant defined
+#define MULTIPLIER 321
+
+// prototype function
+void goosey(int num);
+
+int main() {
+    int userValue;
+
+    // enter an integer value
+    printf("Enter an integer value: ");
+    scanf("%d", &userValue);
+
+    // goosey function
+    goosey(userValue);
+
+    return 0;
+}
+
+// function multiplied the value and symbolic constant
+void goosey(int num) {
+    int result = num * MULTIPLIER;
+    printf("The result of %d multiplied by %d is: %d\n", num, MULTIPLIER, result);
+}
+
+// end
