6aefccc2c3106513117396f9041c5ae9a8771c92
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-15 13:54:36 -0400

    symbolic constant

--- P4/symbolic_constants.c
+++ P4/symbolic_constants.c
@@ -1,30 +1,30 @@
 // !/usr/bin/env Program C
-// Script name: Programming C P3 Conversion
+// Script name: Symbolic Constant P4 
 // Author Name: Christen Reinhart
-// Date of Latest Revision: 09/13/2024
+// Date of Latest Revision: 09/15/2024
 // Purpose: Calculate Area 
 // Input: Side 1/2
 // Output: Area
 
 #include <stdio.h>
 
-#define FACTOR1 2.88  // Symbolic constant for the first multiplier
-#define FACTOR2 3.97  // Symbolic constant for the second multiplier
+#define FACTOR1 2.88  // first multiplier
+#define FACTOR2 3.97  // second multiplier
 
 int main() {
     float side1, side2, area;
 
-    // Prompt user for input for side1 and side2
+    // input for side1 and side2
     printf("Enter the value for side 1: ");
     scanf("%f", &side1);
 
     printf("Enter the value for side 2: ");
     scanf("%f", &side2);
 
-    // Calculate the area of the scona
+    // area of the scona
     area = (side1 * FACTOR1) + (side2 * FACTOR2);
 
-    // Display the result
+    // results displayed
     printf("The area of the scona is: %.2f\n", area);
 
     return 0;
