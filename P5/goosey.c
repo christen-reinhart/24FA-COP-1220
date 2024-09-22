e08b65d778b9f17490ed32639d44d88bb8747667
christen-reinhart <christen.reinhart@yahoo.com>
2024-09-21 20:39:08 -0400

    goosey.c

--- P5/goosey.c
+++ P5/goosey.c
@@ -1,7 +1,7 @@
 // !/usr/bin/env Program C
 // Script name: Programming C P5 
 // Author Name: Christen Reinhart
-// Date of Latest Revision: 09/18/2024
+// Date of Latest Revision: 09/21/2024
 // Purpose: multiplication 
 // Input: type int value
 // Output: 321 Multiplied
