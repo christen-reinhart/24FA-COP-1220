408066242d731944d1f1c6817f46ea455f2f4255
christen-reinhart <christen.reinhart@yahoo.com>
2024-10-25 07:51:02 -0400

    Array

--- P9/Name_Length.c
+++ P9/Name_Length.c
@@ -1,41 +1,41 @@
 // #/!/usr/bin/env Program C
 // Script name: Programming C P9 Name_Length.c
 // Author Name: Christen Reinhart
-// Date of Latest Revision: 10/14/2024
+// Date of Latest Revision: 10/28/2024
 // Purpose: test a program that contains an array of ints
 // Input: characters in 1st & last name
 // Output: contents of the array, along with the total and average
 
 #include <stdio.h>
 
-// Define a named constant for the size of the array (15 for "Christen Reinhart")
+// define the named constant for the size of the array
 #define ARRAY_SIZE 15
 
 int main() {
-    int array[ARRAY_SIZE];  // Declare an array of integers
-    int total = 0;          // Variable to store the total of the values
-    float average = 0.0;    // Variable to store the average of the values
+    int array[ARRAY_SIZE];  // declare array of integers
+    int total = 0;          // variable to store the total value
+    float average = 0.0;    // variable to store the average value
     
-    // Loop to let the user enter values to fill the array
+    // loop for user to enter values for the array
     for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("Enter value %d: ", i + 1);
         while (scanf("%d", &array[i]) != 1) {
             printf("Invalid input. Please enter an integer: ");
-            while (getchar() != '\n'); // Clear the input buffer
+            while (getchar() != '\n'); // clear buffer
         }
-        total += array[i];  // Accumulate the total
+        total += array[i];  // total
     }
     
-    // Calculate the average
+    // calculate average
     average = (float)total / ARRAY_SIZE;
 
-    // Output the contents of the array
+    // array output
     printf("\nArray contents: ");
     for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("%d ", array[i]);
     }
 
-    // Output the total and average
+    // total and average output
     printf("\nTotal: %d", total);
     printf("\nAverage: %.2f\n", average);
 
